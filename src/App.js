e3c1280e16647358227b18a2c3538795a59ca8a5
Tom Kanney <tkanney007@gmail.com>
2023-08-07 22:46:34 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import myPicture from "./../src/assets/gobills.png";
+
+const info = [
+  {
+    sectionName: "About Me",
+    content:
+      "I am a senior level engineer that enjoys developing less experienced engineers through mentorship, pair configuration/programming, and training.",
+  },
+  {
+    sectionName: "Contact Me",
+    content: "thomas.kanney@perkinelmer.com",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img className="meImg" src={myPicture} alt="Picture of Tom Kanney" />
+      <h1>Tom Kanney</h1>
+      <h2>Staff Software Engineer</h2>
+      {info.map((item) => {
+        return (
+          <div className="section">
+            <h3>{item.sectionName}</h3>
+            <hr />
+            <p>{item.content}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
